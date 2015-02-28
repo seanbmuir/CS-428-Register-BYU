041cb43de00a31688cff01d118d684ff510b8ea1
Sydney Shen <shenred176@gmail.com>
2015-02-28 10:06:21 -0700

    clean up useless code of SemesterDAO

--- src/main/java/database/SemesterDAO.java
+++ src/main/java/database/SemesterDAO.java
@@ -24,12 +24,12 @@ public class SemesterDAO implements ISemesterDAO
         this.db = db;
         Jongo jongo = new Jongo(this.db);
         semesters = jongo.getCollection("semester");
-        collection = db.getCollection("semester");
+        //collection = db.getCollection("semester");
     }
     
-    public void createCollection(){
-        db.createCollection("semester", new BasicDBObject("capped", false));
-    }
+    //public void createCollection(){
+    //    db.createCollection("semester", new BasicDBObject("capped", false));
+    //}
     @Override
     public void addSemester(Semester semester)
     {
@@ -40,30 +40,12 @@ public class SemesterDAO implements ISemesterDAO
     @Override
     public void deleteSemester(Semester semester)
     {
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("id", semester.getID());
-        DBCursor cursor = collection.find(searchQuery);
-        while(cursor.hasNext())
-        {
-            collection.remove(cursor.next());
-        }
+    
     }
 
     @Override
     public Semester getSemester(int sem_id)
     {
-//        BasicDBObject searchQuery = new BasicDBObject();
-//        searchQuery.put("id",sem_id);
-//        DBCursor cursor = collection.find(searchQuery);
-//        while (cursor.hasNext())
-//        {
-//            DBObject semesterObject = cursor.next();
-//            String name = semesterObject.get("name").toString();
-//            int id = Integer.parseInt(semesterObject.get("id").toString());
-//            Semester semester = new Semester(name,id);
-//            return semester;
-//        }
-//        return null;
         String query = "{id : "+sem_id+"}"; //TODO use StringBuilder or something else
         Semester semester = semesters.findOne(query).as(Semester.class);
         return semester;
@@ -72,39 +54,19 @@ public class SemesterDAO implements ISemesterDAO
     @Override
     public void addCourses(Courses course)
     {
-        //Model Course needs to be revised
-        BasicDBObject document = new BasicDBObject();
-        //document.put("name",semester.getName());
-        //document.put("id", semester.getID());
-        collection.insert(document);
+
     }
 
     @Override
     public ArrayList<Semester> getSetOfSemester()
     {
-        ArrayList<Semester> semesters = new ArrayList<Semester>();
-        DBCursor cursor = collection.find(); //could have an error here, what set of semester are we looking for?
-        while (cursor.hasNext())
-        {
-            DBObject semesterObject = cursor.next();
-            //Semester semester = new Semester(semesterObject.get("name").toString(),Integer.parseInt(semesterObject.get("id").toString()));
-            //semesters.add(semester);
-        }
-        return semesters;
+        return null;
     }
 
     @Override
     public ArrayList<Course> getCoursesByCredit(int credit)
     {
-        ArrayList<Course> courses = new ArrayList<Course>();
-        DBCursor cursor = collection.find(); //could have an error here, what set of semester are we looking for?
-        while (cursor.hasNext())
-        {
-            DBObject semesterObject = cursor.next();
-            //Semester semester = new Semester(semesterObject.get("name").toString(),semesterObject.get("id"));
-            //semesters.add(semester);
-        }
-        return courses;
+        return null;
     }
 
     @Override
@@ -116,24 +78,13 @@ public class SemesterDAO implements ISemesterDAO
     @Override
     public void addSection(Course course, Section section)
     {
-        //Model Section needs to be revised
-        BasicDBObject document = new BasicDBObject();
-        //document.put("name",semester.getName());
-        //document.put("id", semester.getID());
-        collection.insert(document);
+
     }
 
     @Override
     public void removeSection(Section section)
     {
-        //Model Section needs to be revised
-        BasicDBObject searchQuery = new BasicDBObject();
-        //searchQuery.put("id", semester.getID());
-        DBCursor cursor = collection.find(searchQuery);
-        while(cursor.hasNext())
-        {
-            collection.remove(cursor.next());
-        }
+
     }
 
     public void removeCourse(Course course, int semID)
