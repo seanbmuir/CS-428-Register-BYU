5201b881733b394b3ecac07610ef38488ed3481b
Joe Hoehne <yoehoehne@gmail.com>
2015-03-06 16:15:42 -0700

    Added javadoc to used methods. Moved unused methods to bottom. Reformatted code

--- src/main/java/controllers/PublicWebController.java
+++ src/main/java/controllers/PublicWebController.java
@@ -37,108 +37,247 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/public-api")
-public class PublicWebController {
-  private boolean shuttingDown;
-  private int countdown;
+public class PublicWebController
+{
+    private boolean shuttingDown;
+    private int countdown;
 
-	private PublicWebService webService;
-	private Courses cachedCourses;
-	private List<String> cachedSemesters;
+    private PublicWebService webService;
+    private Courses cachedCourses;
+    private List<String> cachedSemesters;
 
-	public PublicWebController() {
-    countdown = 5;
-    shuttingDown = false;
-		webService = new PublicWebService();
-	}
-
-  @RequestMapping(value = "/shutdown", method = POST)
-  @ResponseStatus(value = HttpStatus.OK)
-  public void shutdown(HttpServletRequest req)
-  {
-    //for temparary testing
-    //System.out.println(req.getRemoteAddr());
-
-    if (!req.getRemoteAddr().matches("172[.]31[.]\\d{1,3}[.]\\d{1,3}|localhost|127[.]0[.]0[.]1")) {
-      throw new ResourceNotFoundException();
+    public PublicWebController()
+    {
+        countdown = 5;
+        shuttingDown = false;
+        webService = new PublicWebService();
     }
-    shuttingDown = true;
-  }
 
-  @RequestMapping(value = "/health", method = GET)
-  @ResponseStatus(value = HttpStatus.OK)
-  public void healthCheck(HttpServletRequest req)
-  {
-    //for temporary testing
-    //System.out.println(req.getRemoteAddr());
 
-    if (!req.getRemoteAddr().matches("172[.]31[.]\\d{1,3}[.]\\d{1,3}|localhost|127[.]0[.]0[.]1")) {
-      throw new ResourceNotFoundException();
+    //TODO:: change this method to return semesters that
+    // are published in the database, SemesterDownloader.getSemesterCodes()
+    // needs to be changed to webServie.getSemesterCodes() (needs to be implemented)
+    // the webService.getSemesterCodes() needs to talk to the DatabaseRegistrationStore/SemesterDAO
+    // and get all of the semesterCodes that are in the database
+    //
+    // The object that is returned as a List<String> of semester codes
+    // ie.  { "20151", "20153" }
+    @RequestMapping(value = "/semesters", method = GET)
+    public
+    @ResponseBody
+    List<String> getSemesters()
+    {
+        if (cachedSemesters == null)
+        {
+            cachedSemesters = SemesterDownloader.getSemesterCodes();
+        }
+        return cachedSemesters;
     }
-    if (shuttingDown) {
-      if (countdown-- <= 0) {
-        System.exit(0);
-      }
-      //ToDo: should probably be changed to a 503 instead of a 500
-      throw new ServerException("Server Shutting Down");
+
+    /**
+     * Returns all courses in all available semesters
+     *
+     * @param semester
+     * @return
+     */
+    @RequestMapping(value = "/courses/all", method = GET)
+    public
+    @ResponseBody
+    Courses getAllCourses(
+            @RequestParam(value = "semester", required = false, defaultValue = "current") String semester)
+    {
+        if ("current".equals(semester))
+        {
+            if (cachedCourses == null)
+            {
+                cachedCourses = webService.getAllCourses();
+            }
+            return cachedCourses;
+        } else
+        {
+            return webService.getCourses(semester);
+        }
     }
-  }
 
-	/**
-	 * Gets all departments that exist in the systen
-	 * @param dummy a boolean stating if you want dummy data
-	 * @return fully populated list of departments
-	 */
-	@RequestMapping(value = "/departments", method = GET)
-	public @ResponseBody
-	Departments getAllDepartments(
-		@RequestParam(value = "dummy", required = false, defaultValue = "false") Boolean dummy)
-	{
-		return webService.getAllDepartments();
-	}
-
-	
-	
-	//TODO:: change this method to return semesters that
-	// are published in the database, SemesterDownloader.getSemesterCodes()
-	// needs to be changed to webServie.getSemesterCodes() (needs to be implemented)
-	// the webService.getSemesterCodes() needs to talk to the DatabaseRegistrationStore/SemesterDAO
-	// and get all of the semesterCodes that are in the database
-	//
-	// The object that is returned as a List<String> of semester codes  
-	// ie.  { "20151", "20153" } 
-	@RequestMapping(value = "/semesters", method = GET)
-	public @ResponseBody
-	List<String> getSemesters()
-	{
-		if(cachedSemesters==null)
-			cachedSemesters = SemesterDownloader.getSemesterCodes();
-		return cachedSemesters;
-	}
-
-  @RequestMapping(value = "/courses/all", method = GET)
-  public @ResponseBody
-  Courses getAllCourses(
-      @RequestParam(value = "semester", required = false, defaultValue ="current") String semester)
-  {
-    if ("current".equals(semester)) {
-	  if( cachedCourses == null )
-	      cachedCourses = webService.getAllCourses();
-	  return cachedCourses;
-    } else {
-      return webService.getCourses(semester);
+    /**
+     * Returns the courses for the requested semester
+     *
+     * @param sem_id
+     * @return
+     */
+    @RequestMapping(value = "/courses/{sem_id}", method = GET)
+    public
+    @ResponseBody
+    Courses getCourses(@PathVariable String sem_id)
+    {
+        return webService.getCourses(sem_id);
     }
-  }
 
-	@RequestMapping(value = "/courses/{sem_id}", method = GET)
-	public @ResponseBody
-	Courses getCourses(@PathVariable String sem_id)
-	{
-		return webService.getCourses(sem_id);
-	}
+
+    /**
+     * Returns all schedules for the signed in student;
+     *
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/schedules/all", method = GET)
+    public
+    @ResponseBody
+    Schedules getSchedulesForUser(HttpSession session)
+    {
+        String uid = getUserId(session);
+        return webService.getAllSchedulesForUser(uid);
+    }
+
+    /**
+     * Saves the given schedule in the database
+     *
+     * @param schedule
+     * @param session
+     */
+    @RequestMapping(value = "/saveSchedule", method = POST,
+            consumes = "application/json")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void addSchedule(
+            @RequestBody Schedule schedule, HttpSession session)
+    {
+        String uid = getUserId(session);
+        webService.addSchedule(uid, schedule);
+    }
+
+    /**
+     * Loads the schedule for the semester
+     *
+     * @param sem_id  the semesterID of the schedule requested
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/loadSchedule", method = GET)
+    public
+    @ResponseBody
+    Schedule
+    getSchedule(@RequestParam(value = "sem_id", required = true) String sem_id, HttpSession session)
+    {
+        String uid = getUserId(session);
+        return webService.getSchedule(uid, sem_id);
+    }
+
+
+    private String getUserId(HttpSession session)
+    {
+        Object uid = session.getAttribute("uid");
+        if (uid == null)
+        {
+            throw new NotAuthorizedException("User must be logged in.");
+        }
+        return (String) uid;
+    }
+
+
+
+
+  /* Potentially Unused Methods =======================================================*/
+
+    @RequestMapping(value = "recaptcha", method = GET)
+    public
+    @ResponseBody
+    String getRecaptcha()
+    {
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader br;
+        String line;
+        StringBuilder result = new StringBuilder();
+        try
+        {
+            url = new URL("https://www.google.com/recaptcha/api/challenge?k=6LfoisoSAAAAAFBP_LvBQ4YlpPTBOf12MnGsjk4z");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            InputStreamReader reader = new InputStreamReader(conn.getInputStream());
+            String response = IOUtils.toString(reader);
+            reader.close();
+            List<Integer> braceIndexes = new ArrayList<>(2);
+            boolean inQuotes = false, jsonObjectFound = false;
+            char closingQuotesChar = '\0';
+            for (int i = 0; i < response.length() && !jsonObjectFound; i++)
+            {
+                if (!inQuotes)
+                {
+                    switch (response.charAt(i))
+                    {
+                        case '"':
+                        case '\'':
+                            inQuotes = true;
+                            closingQuotesChar = response.charAt(i);
+                            break;
+                        case '{':
+                        case '}':
+                            braceIndexes.add(i);
+                            if (braceIndexes.size() == 2)
+                            {
+                                jsonObjectFound = true;
+                            }
+                            break;
+
+                    }
+                } else if (closingQuotesChar == response.charAt(i))
+                {
+                    inQuotes = false;
+                }
+            }
+            if (braceIndexes.size() != 2)
+            {
+                throw new RuntimeException("Google returned invalid JSON: " + response);
+            }
+            JSONObject googleJSON = new JSONObject(response.substring(braceIndexes.get(0), braceIndexes.get(1) + 1));
+            JSONObject ourJSON = new JSONObject();
+            ourJSON.put("challenge", googleJSON.get("challenge"));
+            ourJSON.put("image", getRecaptchaImage(googleJSON.get("challenge").toString()));
+
+            return ourJSON.toString();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return e.getMessage();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * Gets all departments that exist in the systen
+     *
+     * @param dummy a boolean stating if you want dummy data
+     * @return fully populated list of departments
+     */
+    @RequestMapping(value = "/departments", method = GET)
+    public
+    @ResponseBody
+    Departments getAllDepartments(
+            @RequestParam(value = "dummy", required = false, defaultValue = "false") Boolean dummy)
+    {
+        return webService.getAllDepartments();
+    }
+
+    String getRecaptchaImage(String recaptchaChallenge) throws IOException
+    {
+        URL url = new URL("https://www.google.com/recaptcha/api/image?c=" + recaptchaChallenge);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        InputStream is = conn.getInputStream();
+        String result = new String(Base64.encodeBase64(IOUtils.toByteArray(is)), "UTF-8");
+        is.close();
+        return result;
+    }
+
 
     @RequestMapping(value = "/register", method = POST)
-    public String registerSchedule(@RequestBody String courseDetails, HttpSession session) {
-      session.setAttribute("CourseInfo", courseDetails);
+    public String registerSchedule(@RequestBody String courseDetails, HttpSession session)
+    {
+        session.setAttribute("CourseInfo", courseDetails);
         System.out.println(courseDetails);
         return "redirect:https://cas.byu.edu/cas/login?service=http://andyetitcompiles.com/" +
                 "public-api/register/handle?courseInfo="
@@ -146,141 +285,61 @@ public class PublicWebController {
     }
 
     @RequestMapping(value = "/register/handle")
-    public @ResponseBody
+    public
+    @ResponseBody
     String handleRegistration(
             @RequestParam String ticket, HttpSession session)
     {
-      String courseInfo = (String) session.getAttribute("CourseInfo");
+        String courseInfo = (String) session.getAttribute("CourseInfo");
         webService.handleRegistration(courseInfo, ticket);
         return "ok " + courseInfo + " " + ticket;
     }
 
 
-  @RequestMapping(value = "/schedules/all", method = GET)
-  public @ResponseBody
-  Schedules getSchedulesForUser(HttpSession session)
-  {
-    String uid = getUserId(session);
-    return webService.getAllSchedulesForUser(uid);
-  }
-
-  @RequestMapping(value = "/schedules", method = POST,
-      consumes = "application/json")
-  @ResponseStatus(value = HttpStatus.OK)
-  public void addSchedule(
-      @RequestBody Schedule schedule, HttpSession session)
-  {
-    String uid = getUserId(session);
-    webService.addSchedule(uid, schedule);
-  }
-
-  @RequestMapping(value = "/schedules/{id}", method = PUT,
-      consumes = "application/json")
-  @ResponseStatus(value = HttpStatus.OK)
-  public void updateSchedule(@RequestBody Schedule schedule,
-       @PathVariable String id, HttpSession session)
-  {
-    String uid = getUserId(session);
-    webService.editSchedule(uid, id, schedule);
-  }
-
-  @RequestMapping(value = "schedules/{id}", method = DELETE,
-    consumes = "application/json")
-  @ResponseStatus(value = HttpStatus.OK)
-  public void removeSchedule(@PathVariable String id, HttpSession session)
-  {
-    String uid = getUserId(session);
-    webService.removeSchedule(uid, id);
-  }
-
-  @RequestMapping(value = "schedules/{id}", method = GET)
-  public @ResponseBody Schedule
-  getSchedule(@PathVariable String id, HttpSession session)
-  {
-    String uid = getUserId(session);
-    return webService.getSchedule(uid, id);
-  }
-
-  @RequestMapping(value = "recaptcha", method = GET)
-  public @ResponseBody String getRecaptcha()
-  {
-	  URL url;
-	  HttpURLConnection conn;
-	  BufferedReader br;
-	  String line;
-	  StringBuilder result = new StringBuilder();
-	  try {
-		  url = new URL("https://www.google.com/recaptcha/api/challenge?k=6LfoisoSAAAAAFBP_LvBQ4YlpPTBOf12MnGsjk4z");
-		  conn = (HttpURLConnection) url.openConnection();
-		  conn.setRequestMethod("GET");
-		  InputStreamReader reader = new InputStreamReader(conn.getInputStream());
-		  String response = IOUtils.toString(reader);
-		  reader.close();
-		  List<Integer> braceIndexes = new ArrayList<>(2);
-		  boolean inQuotes = false, jsonObjectFound=false;
-		  char closingQuotesChar = '\0';
-		  for( int i=0; i<response.length() && !jsonObjectFound; i++ )
-		  {
-			  if( !inQuotes ) {
-				  switch (response.charAt(i)) {
-					  case '"':
-					  case '\'':
-						  inQuotes = true;
-						  closingQuotesChar = response.charAt(i);
-						  break;
-					  case '{':
-					  case '}':
-						  braceIndexes.add(i);
-						  if( braceIndexes.size() == 2 )
-							  jsonObjectFound = true;
-						  break;
-
-				  }
-			  } else if( closingQuotesChar == response.charAt(i) ) {
-				  inQuotes = false;
-			  }
-		  }
-		  if( braceIndexes.size() != 2 )
-			  throw new RuntimeException("Google returned invalid JSON: "+response);
-		  JSONObject googleJSON = new JSONObject(response.substring(braceIndexes.get(0), braceIndexes.get(1)+1));
-		  JSONObject ourJSON = new JSONObject();
-		  ourJSON.put("challenge", googleJSON.get("challenge"));
-		  ourJSON.put("image", getRecaptchaImage(googleJSON.get("challenge").toString()));
-
-		  return ourJSON.toString();
-	  } catch (IOException e) {
-		  e.printStackTrace();
-		  return e.getMessage();
-	  } catch (Exception e) {
-		  e.printStackTrace();
-		  return e.getMessage();
-	  }
-  }
-
-  private String getUserId(HttpSession session) {
-    Object uid = session.getAttribute("uid");
-    if (uid == null) {
-      throw new NotAuthorizedException("User must be logged in.");
+    @RequestMapping(value = "dbupdated", method = GET)
+    public
+    @ResponseBody
+    String dbUpdated()
+    {
+        cachedCourses = null;
+        cachedSemesters = null;
+        return "success";
     }
-    return (String) uid;
-  }
 
-	String getRecaptchaImage(String recaptchaChallenge) throws IOException {
-		URL url = new URL("https://www.google.com/recaptcha/api/image?c=" + recaptchaChallenge);
+    @RequestMapping(value = "/shutdown", method = POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void shutdown(HttpServletRequest req)
+    {
+        //for temparary testing
+        //System.out.println(req.getRemoteAddr());
 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		InputStream is = conn.getInputStream();
-		String result = new String(Base64.encodeBase64(IOUtils.toByteArray(is)), "UTF-8");
-		is.close();
-		return result;
-	}
+        if (!req.getRemoteAddr().matches("172[.]31[.]\\d{1,3}[.]\\d{1,3}|localhost|127[.]0[.]0[.]1"))
+        {
+            throw new ResourceNotFoundException();
+        }
+        shuttingDown = true;
+    }
 
-	@RequestMapping(value="dbupdated", method=GET)
-	public @ResponseBody String dbUpdated() {
-		cachedCourses = null;
-		cachedSemesters = null;
-		return "success";
-	}
+    @RequestMapping(value = "/health", method = GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void healthCheck(HttpServletRequest req)
+    {
+        //for temporary testing
+        //System.out.println(req.getRemoteAddr());
+
+        if (!req.getRemoteAddr().matches("172[.]31[.]\\d{1,3}[.]\\d{1,3}|localhost|127[.]0[.]0[.]1"))
+        {
+            throw new ResourceNotFoundException();
+        }
+        if (shuttingDown)
+        {
+            if (countdown-- <= 0)
+            {
+                System.exit(0);
+            }
+            //ToDo: should probably be changed to a 503 instead of a 500
+            throw new ServerException("Server Shutting Down");
+        }
+    }
 
 }
